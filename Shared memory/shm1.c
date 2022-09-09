e76e5e95e4499ca956424248ce8105ed2e937909
Ilya Trishin <mutantick@gmail.com>
2022-09-09 19:43:13 +0700

    shared mmry

---
+++ Shared memory/shm1.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <errno.h>
+#include <string.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/wait.h> 
+#include <sys/types.h>
+#include <sys/mman.h>
+#include <unistd.h>
+
+#define SHM_SIZE 1024
+#define MY_SHM "my_shm"
+
+int main()
+{
+	int shm_fd;
+	char *vaddr;
+
+	if ((shm_fd = shm_open(MY_SHM, O_CREAT | O_RDWR, 0666)) == -1)
+	{
+		perror("cannot open");
+		exit(EXIT_FAILURE);
+	}
+
+	if (ftruncate(shm_fd, SHM_SIZE) != 0)
+	{
+		perror("cannot set size");
+		exit(EXIT_FAILURE);
+	}
+
+	if ((vaddr = mmap(0, SHM_SIZE, PROT_WRITE, MAP_SHARED, shm_fd, 0)) == MAP_FAILED)
+	{
+		perror("cannot mmap");
+		exit(EXIT_FAILURE);
+	}
+
+	strcpy(vaddr,"Hello!");
+
+	if (mlock(vaddr, SHM_SIZE) != 0)
+	{
+		perror("cannot mlock");
+		exit(EXIT_FAILURE);
+	}
+
+	munmap(vaddr, SHM_SIZE);
+	close(shm_fd);
+	//shm_unlink(MY_SHM);
+	
+	exit(EXIT_SUCCESS);
+}
